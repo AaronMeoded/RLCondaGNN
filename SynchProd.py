91ac7b4779967539ac698e968e7d70674a8926be
Aaron Meoded <aaron.meoded@student.carmel.edu.hk>
2023-07-18 13:06:05 +0300

    packages set up

---
+++ SynchProd.py
@@ -0,0 +1,56 @@
+import mkl
+import ctypes
+# print(mkl.__version__)
+# import pm4py
+from pm4py.objects.petri_net.obj import PetriNet, Marking
+from pm4py.objects.petri_net.utils.petri_utils import add_arc_from_to
+from pm4py.objects.log.obj import Trace, Event
+# from pm4py.objects.petri_net.sync_product import construct
+# from pm4py.algo.conformance.alignments.petri_net import get_best_worst_cost, apply as align
+from pm4py.algo.conformance.alignments.petri_net import algorithm as align
+
+# Step 1: Define the Petri net
+net = PetriNet('Net')
+
+# Add places
+p1 = PetriNet.Place('p1')
+p2 = PetriNet.Place('p2')
+p3 = PetriNet.Place('p3')
+net.places.add(p1)
+net.places.add(p2)
+net.places.add(p3)
+
+# Add transitions
+t1 = PetriNet.Transition('t1', 'A')
+t2 = PetriNet.Transition('t2', 'B')
+net.transitions.add(t1)
+net.transitions.add(t2)
+
+# Add arcs
+add_arc_from_to(p1, t1, net)
+add_arc_from_to(t1, p2, net)
+add_arc_from_to(p2, t2, net)
+add_arc_from_to(t2, p3, net)
+
+# Initial and final marking
+initial_marking = Marking()
+final_marking = Marking()
+initial_marking[p1] = 1
+final_marking[p3] = 1
+
+# Step 2: Define the trace
+trace = Trace()
+trace.append(Event({'concept:name': 'A'}))
+trace.append(Event({'concept:name': 'A'}))
+trace.append(Event({'concept:name': 'B'}))
+
+# # Step 3: Compute synchronous product
+# sync_net, sync_initial_marking, sync_final_marking = construct(net, initial_marking, final_marking, trace)
+
+# Parameters for alignment
+parameters = {"ret_tuple_as_trans_desc": True}
+
+# Step 3: Compute alignment
+aligned_trace = align(trace, net, initial_marking, final_marking, parameters=parameters)
+if __name__ == "__main__":
+    print(aligned_trace)
--- main.py
+++ main.py
@@ -1,11 +1,41 @@
+# import mkl
 import torch
 from torch_geometric.nn import GCNConv
 import torch_geometric as tg
 import numpy as np
 
+
+# data preprocessing. #1 load dataset. #2. Inductive miner to get process model.
+#                     #3 Convert to PyG friendly format. #4 Figure out how to construct on the fly.
 # Preprocess your graph data into PyTorch Geometric format
 # This is a place holder and you will need to replace it with your own data
-data = tg.data.Data(x=torch.randn(100, num_node_features), edge_index=torch.randint(100, (2, 500)))
+# data = tg.data.Data(x=torch.randn(100, num_node_features), edge_index=torch.randint(100, (2, 500)))
+
+# dataset = 'prBm6'
+#
+# # load dataset:
+# df = load_dataset(dataset, unique_traces=True)
+# df = df[['case:concept:name', 'concept:name']]
+# df_no_loops = df.reset_index()
+#
+# # inductive miner:
+# has_model = ['pr_1151_clean', 'pr_1151_noise', 'pr_1912_clean', 'pr_1912_noise', 'prAm6', 'prBm6', 'prCm6', 'prDm6', 'prEm6', 'prFm6', 'prGm6']
+#     if dataset in has_model:
+#         has_model_flag = True
+#         df_no_loops['concept:name'] = df_no_loops['concept:name'] + '+complete'
+#         net, init_marking, final_marking = load_model(dataset)
+#     else:
+#         has_model_flag = False
+#         df_train, df_test = train_test_log_split(df_no_loops, n_traces=n_train_traces,
+#                                                  random_selection=random_trace_selection,
+#                                                  sample_from_each_trace_family=sample_from_each_trace_family,
+#                                                  n=n_samples_from_each_family, random_seed=random_seed)
+#         net, init_marking, final_marking = inductive_miner.apply(df_train)
+#
+#     model = from_discovered_model_to_PetriNet2(net, init_marking, final_marking, has_model_flag,
+#                                                non_sync_move_penalty=non_sync_penalty, cost_function=deterministic,
+#                                                conditioned_prob_compute=conditioned_prob_compute)
+
 
 # simple 2-layer GCN
 class GCN(torch.nn.Module):
@@ -58,6 +88,7 @@ def reinforce(policy_network, optimizer, num_episodes, discount_factor=0.99):
         state = env.reset()
 
         # Generate an episode
+        # make range(100) = |trace|+|model|
         for t in range(100):  # assuming maximum length of episode is 100
             action = select_action(policy_network, state)
             next_state, reward, done, _ = env.step(action)
@@ -83,7 +114,33 @@ def reinforce(policy_network, optimizer, num_episodes, discount_factor=0.99):
 
 
 # Define your environment here
-env = ...
+class GraphEnv:
+    def __init__(self, initial_state, final_state):
+        self.initial_state = initial_state
+        self.current_state = initial_state
+        self.final_state = final_state
+
+    def reset(self):
+        self.current_state = self.initial_state
+        return self.current_state
+
+    def step(self, action):
+        """
+        Apply the given action and update the current_state,
+        calculate the reward and check if the episode is done.
+        """
+        next_state = action.endNode  # Update your state based on the action
+        reward = action.weight  # Calculate the reward for the taken action
+        # Check if the episode has finished
+        done = False
+        if self.current_state == self.final_state:
+            done = True
+        self.current_state = next_state
+
+        return next_state, reward, done
+
+
+
 
 # Create network and optimizer
 # The number of features for each node in your graph.
@@ -92,6 +149,9 @@ num_node_features = 10
 hidden_channels = 16
 num_classes = 10
 num_actions = 3
+initial_state = 0
+final_state = 10
+env = GraphEnv(initial_state, final_state)
 gcn = GCN(num_node_features, hidden_channels, num_classes)
 policy_network = PolicyNetwork(gcn)
 optimizer = torch.optim.Adam(policy_network.parameters(), lr=0.01)
@@ -99,130 +159,3 @@ optimizer = torch.optim.Adam(policy_network.parameters(), lr=0.01)
 
 # Train
 reinforce(policy_network, optimizer, num_episodes=100)
-
-
-
-
-# import torch
-# from torch_geometric.nn import GCNConv
-#
-
-# # implement reinforce
-# def reinforce(policy_network, optimizer, episodes):
-#     for episode in episodes:
-#         total_reward = 0
-#         log_probs = []
-#
-#         # Collect trajectory
-#         for step in episode:
-#             data, action, reward = step
-#             action_probs = policy_network(data)
-#             log_prob = torch.log(action_probs[action])
-#             log_probs.append(log_prob)
-#             total_reward += reward
-#
-#         # Calculate loss
-#         loss = (-total_reward) * torch.cat(log_probs).sum()
-#
-#         # Update weights
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()import torch
-# from torch_geometric.nn import GCNConv
-#
-# # simple 2-layer GCN
-# class GCN(torch.nn.Module):
-#     def __init__(self, num_node_features, hidden_channels, num_classes):
-#         super(GCN, self).__init__()
-#         self.conv1 = GCNConv(num_node_features, hidden_channels)
-#         self.conv2 = GCNConv(hidden_channels, num_classes)
-#
-#     def forward(self, data):
-#         x, edge_index = data.x, data.edge_index
-#
-#         x = self.conv1(x, edge_index)
-#         x = x.relu()
-#         x = self.conv2(x, edge_index)
-#
-#         return x
-#
-#
-# # define policy network
-# class PolicyNetwork(torch.nn.Module):
-#     def __init__(self, gcn):
-#         super(PolicyNetwork, self).__init__()
-#         self.gcn = gcn
-#         self.action_head = torch.nn.Sequential(
-#             torch.nn.Linear(num_classes, num_actions),
-#             torch.nn.Softmax(dim=-1)
-#         )
-#
-#     def forward(self, data):
-#         x = self.gcn(data)
-#         action_probs = self.action_head(x)
-#         return action_probs
-#
-#
-# # implement reinforce
-# def reinforce(policy_network, optimizer, episodes):
-#     for episode in episodes:
-#         total_reward = 0
-#         log_probs = []
-#
-#         # Collect trajectory
-#         for step in episode:
-#             data, action, reward = step
-#             action_probs = policy_network(data)
-#             log_prob = torch.log(action_probs[action])
-#             log_probs.append(log_prob)
-#             total_reward += reward
-#
-#         # Calculate loss
-#         loss = (-total_reward) * torch.cat(log_probs).sum()
-#
-#         # Update weights
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-#
-#
-# # train model
-#
-# # Create network and optimizer
-# # The number of features for each node in your graph.
-# # This needs to be the same as the dimension of the feature vectors that you associate with each node.
-# num_node_features = 10
-# hidden_channels = 16
-# num_classes = 10
-# num_actions = 3
-# gcn = GCN(num_node_features, hidden_channels, num_classes)
-# policy_network = PolicyNetwork(gcn)
-# optimizer = torch.optim.Adam(policy_network.parameters(), lr=0.01)
-#
-# # Generate your episodes here
-# episodes = ...
-#
-#
-# # Train
-# reinforce(policy_network, optimizer, episodes)
-#
-#
-# # train model
-#
-# # Create network and optimizer
-# # The number of features for each node in your graph.
-# # This needs to be the same as the dimension of the feature vectors that you associate with each node.
-# num_node_features = 10
-# hidden_channels = 16
-# num_classes = 10
-# num_actions = 3
-# gcn = GCN(num_node_features, hidden_channels, num_classes)
-# policy_network = PolicyNetwork(gcn)
-# optimizer = torch.optim.Adam(policy_network.parameters(), lr=0.01)
-#
-# # Generate your episodes here
-# episodes = ...
-#
-#
-# # Train
-# reinforce(policy_network, optimizer, episodes)
