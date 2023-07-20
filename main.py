d6862cbc6b7ccfeb995df027e3c8fb725c897828
Aaron Meoded <aaron.meoded@student.carmel.edu.hk>
2023-07-20 10:47:46 +0300

    works for trivial eexamplegit add .

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# import mkl
 import torch
 from torch_geometric.nn import GCNConv
 import torch_geometric as tg
@@ -28,23 +27,28 @@ class PolicyNetwork(torch.nn.Module):
     def __init__(self, gcn, num_actions):
         super(PolicyNetwork, self).__init__()
         self.gcn = gcn
-        self.action_head = torch.nn.Sequential(
-            torch.nn.Linear(num_classes, num_actions),
-            torch.nn.Softmax(dim=-1)
-        )
+        self.action_head = torch.nn.Linear(num_classes, num_actions)
+        # self.action_head = torch.nn.Sequential(
+        #     torch.nn.Linear(num_classes, num_actions),
+        #     torch.nn.Softmax(dim=-1)
+        # )
 
     def forward(self, state, valid_moves):
         x = self.gcn(data)[state]
         action_probs = self.action_head(x)[valid_moves]
+        # action_probs = action_probs / action_probs.sum()  # Renormalize so the probabilities sum to 1
         return action_probs
 
 
 # Define epsilon-greedy strategy
 def select_action(policy_network, state, valid_moves, epsilon=0.1):
+    # print(valid_moves)
     with torch.no_grad():
         action_probs = policy_network(state, valid_moves)
-    if np.random.uniform(0, 1) < epsilon:
-        action = np.random.choice(valid_moves)
+    action_probs = action_probs / action_probs.sum()  # Renormalize so the probabilities sum to 1
+    if torch.rand(1).item() < epsilon:
+        action = np.random.choice(valid_moves)  # change here: select from the range of valid_moves
+        # action = valid_moves[torch.randint(len(valid_moves), (1,)).item()]
     else:
         action = valid_moves[action_probs.argmax().item()]
     return action
@@ -56,32 +60,80 @@ def reinforce(policy_network, optimizer, num_episodes, discount_factor=0.99):
         log_probs = []
         rewards = []
         state = env.reset()
-        valid_moves = env.outgoing_edges()
+        # valid_moves = env.outgoing_edges()
+        # print(valid_moves)
 
         # Generate an episode
         # make range(100) = |trace|+|model|
         for t in range(100):  # assuming maximum length of episode is 100
-            action = select_action(policy_network, state, valid_moves)
-            next_state, reward, done = env.step(action)
-            log_prob = torch.log(policy_network(state, valid_moves)[action])
+            valid_moves = env.outgoing_edges()
+            action = select_action(policy_network, state, valid_moves)  # action means edge to take next
+            # print('action: ')
+            # print(action)
+            # print('valid_moves: ')
+            # print(valid_moves)
+            # print('PN: ')
+            # print(policy_network(state, valid_moves)[valid_moves.index(action)])
+            # print(torch.log(policy_network(state, valid_moves)[valid_moves.index(action)]))
+            log_prob = torch.log(policy_network(state, valid_moves)[valid_moves.index(action)])
+            # log_prob = torch.log(policy_network(state, valid_moves)[action].unsqueeze(0))
             log_probs.append(log_prob)
+            next_state, reward, done = env.step(action)
+            state = next_state
+            # print('new state: ')
+            # print(state)
             rewards.append(reward)
             if done:
                 break
-            state = next_state
 
         # Update policy
-        discounts = [discount_factor ** i for i in range(len(rewards) + 1)]
-        R = sum([a * b for a, b in zip(discounts, rewards)])
 
+        # discounts = [discount_factor ** i for i in range(len(rewards))]
+        # R = sum([a * b for a, b in zip(discounts, rewards)])
+        # policy_loss = []
+        # for log_prob in log_probs:
+        #     policy_loss.append(-log_prob * R)
+        # policy_loss = torch.stack(policy_loss).sum()
+
+        # Calculate returns from each time step
+        returns = []
+        G = 0
+        for reward in rewards[::-1]:
+            G = reward + discount_factor * G
+            returns.insert(0, G)
+
+        # Normalize returns
+        returns = torch.tensor(returns)
+        returns = (returns - returns.mean()) / (returns.std() + 1e-9)
+
+        # Calculate policy loss
         policy_loss = []
-        for log_prob in log_probs:
-            policy_loss.append(-log_prob * R)
-        policy_loss = torch.cat(policy_loss).sum()
+        for log_prob, G in zip(log_probs, returns):
+            policy_loss.append(-log_prob * G)
+        policy_loss = torch.stack(policy_loss).sum()
 
+        print(f"Summed Rewards Episode #{i_episode}: {sum(rewards)}")
         optimizer.zero_grad()
         policy_loss.backward()
         optimizer.step()
+
+    # Zero-epsilon Run
+    rewards = []
+    state = env.reset()
+    for t in range(100):  # assuming maximum length of episode is 100
+        valid_moves = env.outgoing_edges()
+        action = select_action(policy_network, state, valid_moves, epsilon=0)  # action means edge to take next
+        log_prob = torch.log(policy_network(state, valid_moves)[valid_moves.index(action)])
+        # log_prob = torch.log(policy_network(state, valid_moves)[action].unsqueeze(0))
+        log_probs.append(log_prob)
+        next_state, reward, done = env.step(action)
+        state = next_state
+        # print('new state: ')
+        # print(state)
+        rewards.append(reward)
+        if done:
+            break
+    print(f"Zero-epsilon Summed Rewards Episode #{i_episode}: {sum(rewards)}")
 
 
 # Define your environment here
@@ -111,9 +163,9 @@ class GraphEnv:
         reward = self.data.edge_attr[action].item()  # Calculate the reward for the taken action
         # Check if the episode has finished
         done = False
+        self.current_state = next_state
         if self.current_state == self.final_state:
             done = True
-        self.current_state = next_state
 
         return next_state, reward, done
 
@@ -121,13 +173,15 @@ class GraphEnv:
 # map log, model or synchronous move to weight in the graph
 def cost(action):
     if "<<" in action:
-        return (1+10**-5)*-1
-    return (10**-10)*-1
+        # return (1+10**-5)*-1
+        return -1
+    # return (10**-10)*-1
+    return 2
 
 
 # Example of a graph:
 # Edge pairs
-edges = [(0, 1), (0, 2), (0, 3), (1, 3), (2, 3), (2, 4), (3, 5), (2, 5), (4, 5)]
+edges = [(0, 1), (0, 2), (0, 3), (1, 3), (2, 3), (2, 4), (3, 5), (4, 5)]
 
 # Convert edge pairs to tensor for PyG graph
 edge_index = torch.tensor(edges, dtype=torch.long).t().contiguous()
@@ -171,8 +225,8 @@ env = GraphEnv(data, initial_state, final_state)
 gcn = GCN(num_node_features, hidden_channels, num_classes)
 num_actions = data.edge_index.shape[1]  # Number of edges in the graph
 policy_network = PolicyNetwork(gcn, num_actions)
-optimizer = torch.optim.Adam(policy_network.parameters(), lr=0.01)
+optimizer = torch.optim.Adam(policy_network.parameters(), lr=0.03)
 
 
 # Train
-reinforce(policy_network, optimizer, num_episodes=100)
+reinforce(policy_network, optimizer, num_episodes=500)
